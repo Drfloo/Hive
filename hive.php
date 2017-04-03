e7b06bb4a820df5936774a4bcbb598739431521e
Damien10675 <damien-barber@outlook.com>
2017-04-03 12:32:09 +0200

    debut ajout creation bdd

--- hive.php
+++ hive.php
@@ -38,8 +38,21 @@ class Hive extends Module
         return true;
     }
 
+    function createDB(){
+       /* Db::getInstance()->Execute('
+             CREATE TABLE IF NOT EXISTS `'._DB_PREFIX_.'hive_bdd` (
+             `id_hive_bdd` int(11) NOT NULL,
+             `title` varchar(255) NOT NULL,
+             `content` text NOT NULL,
+             `position` varchar(32) NOT NULL,
+             `id_lang` int(11) NOT NULL,
+             KEY `id_superblock` (`id_superblock`)
+             )ENGINE=MyISAM DEFAULT CHARSET=latin1;'); */
+        }
+
     public function uninstall()
     {
+       // Db::getInstance()->Execute('DROP TABLE `'._DB_PREFIX_.'hive_bdd`');
         if (!parent::uninstall())
             return false;
 
