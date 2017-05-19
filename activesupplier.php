8370d8252d75485a879c9492a62e7406775fc0f2
Drfloo <florentbruziaux@gmail.com>
2017-05-19 12:08:39 +0200

    Fix

--- activesupplier.php
+++ activesupplier.php
@@ -15,24 +15,4 @@ Db::getInstance()->update('hive_bdd',[
     'supplier_enabled'  => $_POST['statut'],
 ],'`id_supplier` = '.$_POST['id_supplier'].' AND `id_product_attribute` = '.$_POST['id']);
 
-$sql = "SELECT id_supplier,supplier_enabled,supplier_default
-        FROM ps_hive_bdd 
-        WHERE id_product = 6 
-        AND id_product_attribute = 31";
 
-$sql = Db::getInstance()->executeS($sql);
-foreach ($sql as $row) {
-    $result = array(
-      'id_supplier' => $row['id_supplier'],
-      'supplier_enabled' => $row['supplier_enabled'],
-      'supplier_default' => $row['supplier_default']
-    );
-    $supplier_enabled = $result['supplier_enabled'];
-    $supplier_default = $result['supplier_default'];
-
-    if($supplier_enabled = 0 ){
-        $supplier_default = 0;
-        Db::getInstance()->update('table',
-            array('supplier_enabled'=>'0', 'supplier_default' => 0 ));
-    }
-}
